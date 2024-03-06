0adb29e3ff56ba2302de1f76dc983cab26256f24
Daniel Fernández <daniel@dfer.io>
2024-03-06 01:00:15 +0100

    Normalize and clean audio, convert numbers to text
    Signed-off-by: Daniel Fernández <daniel@dfer.io>

--- gen_dataset.py
+++ gen_dataset.py
@@ -2,12 +2,22 @@
 
 import os
 import sys
-import torch
+import re
+#import torch
+import librosa
+import num2words
+import tempfile
+import shutil
+import demucs.api
+import string
+import random
+import argparse
+
 from tacotron2 import layers
 import numpy as np
-from pydub import AudioSegment
+from pydub import AudioSegment, effects  
 from pydub.silence import split_on_silence
-from scipy.io.wavfile import read as read_wav
+from scipy.io.wavfile import read as read_wav, write as write_wav
 from tacotron2 import hparams as t_hparams
 
 hparams = None
@@ -22,23 +32,45 @@ def init_globals():
                                hparams.n_mel_channels, hparams.sampling_rate, hparams.mel_fmin,
                                hparams.mel_fmax)
 
-def load_wav_to_torch(full_path):
-    sampling_rate, data = read_wav(full_path)
-    return torch.FloatTensor(data.astype(np.float32)), sampling_rate
+def preprocess_audio(wav_file, silence_audio_size):
+    sr = hparams.sampling_rate
+    max_wav_value = 32768.0
+    trim_fft_size = 1024
+    trim_hop_size = 256
+    trim_top_db   = 23
 
-def wav2mel(input_filename, output_filename):
-    print("Generating mel")
+    
+    data, sampling_rate = librosa.core.load(wav_file, sr)
+    silence_data = [0.] * silence_audio_size
+    data_tmp = data / np.abs(data).max() * 0.999
+    data_tmp = np.append(silence_data, data_tmp)
+    data_tmp = librosa.effects.trim(data_tmp, top_db=trim_top_db, frame_length=trim_fft_size, hop_length=trim_hop_size)[0]
+    data_tmp = data_tmp * max_wav_value
+    data_tmp = np.append(data_tmp, silence_data)
+    data_tmp = data_tmp.astype(dtype=np.int16)
+    write_wav(wav_file, sr, data_tmp)
+    #print(len(data),len(data_))
+    #if(i%100 == 0):
+    #    print (i)
 
-    audio, sampling_rate = load_wav_to_torch(input_filename)
-    if sampling_rate != stft.sampling_rate:
-        raise ValueError("{} {} SR does not match the objective {} SR".format(filename,
-            sampling_rate, stft.sampling_rate))
-    audio_norm = audio / hparams.max_wav_value
-    audio_norm = audio_norm.unsqueeze(0)
-    audio_norm = torch.autograd.Variable(audio_norm, requires_grad=False)
-    melspec = stft.mel_spectrogram(audio_norm)
-    melspec = torch.squeeze(melspec, 0).cpu().numpy()
-    np.save(output_filename, melspec)
+
+#def load_wav_to_torch(full_path):
+#    sampling_rate, data = read_wav(full_path)
+#    return torch.FloatTensor(data.astype(np.float32)), sampling_rate
+#
+#def wav2mel(input_filename, output_filename):
+#    #print("Generating mel")
+#
+#    audio, sampling_rate = load_wav_to_torch(input_filename)
+#    if sampling_rate != stft.sampling_rate:
+#        raise ValueError("{} {} SR does not match the objective {} SR".format(filename,
+#            sampling_rate, stft.sampling_rate))
+#    audio_norm = audio / hparams.max_wav_value
+#    audio_norm = audio_norm.unsqueeze(0)
+#    audio_norm = torch.autograd.Variable(audio_norm, requires_grad=False)
+#    melspec = stft.mel_spectrogram(audio_norm)
+#    melspec = torch.squeeze(melspec, 0).cpu().numpy()
+#    np.save(output_filename, melspec)
 
 
 def range_srt2millis(audio_txt):
@@ -71,72 +103,173 @@ def change_extension(filename, new_ext):
     return result_filename
 
 
-def audio_srt_to_chunks(speaker, audio_filename, outdir):
+def convert_num_to_words(utterance, lang="es"):
+    utterance = ' '.join([num2words.num2words(i, lang=lang) if i.isdigit() else i for i in utterance.split()])
+    return utterance
+
+def extract_nums_from_text(input_text, lang):
+    return convert_num_to_words(re.sub('(\d+(\.\d+)?)', r' \1 ', input_text), lang=lang).strip()
+
+def extract_vocals(input_audio_file, output_audio_file):
+
+    separator = demucs.api.Separator(model="mdx_extra", segment=12, progress=True)
+    orig, separated = separator.separate_audio_file(input_audio_file)
+    demucs.api.save_audio(separated["vocals"], output_audio_file, samplerate=separator.samplerate)
+
+def audio_srt_to_chunks(speaker, audio_filename_in, lang, outdir, noise_remove=False):
     audio_subdir = "audio"
     #dataset_path = os.path.join(outdir, dataset_name)
     dataset_path = outdir
     audio_out_dir = os.path.join(dataset_path, audio_subdir)
     dataset_filename_wav = os.path.join(dataset_path, "dataset_wav.txt")
-    dataset_filename_npy = os.path.join(dataset_path, "dataset_npy.txt")
+    #dataset_filename_npy = os.path.join(dataset_path, "dataset_npy.txt")
+    index_filename       = os.path.join(dataset_path, "index.txt")
     os.makedirs(dataset_path, exist_ok=True)
     os.makedirs(audio_out_dir, exist_ok=True)
-    audio_obj = AudioSegment.from_file(audio_filename).set_frame_rate(hparams.sampling_rate).set_channels(1)
-   
-    srt_filename = change_extension(audio_filename, "srt")
 
-    with open(srt_filename, "r") as f:
-        srt_lines = f.readlines()
+    with tempfile.TemporaryDirectory() as tmp_dir:
+        tmp_file_name = ''.join(random.choices(string.ascii_lowercase, k=4)) + ".wav"
 
-    out_lines_wav = list()
-    out_lines_npy = list()
+        if noise_remove:
+            audio_filename = os.path.join(tmp_dir, tmp_file_name)
+            print("Isolating vocal part using demucs...")
+            extract_vocals(audio_filename_in, audio_filename)
+        else:
+            audio_filename = audio_filename_in
 
-    sequence = 0
-    total_lines = len(srt_lines)
-    for i in range(0, total_lines, 4):
-       idx            = srt_lines[i]
-       time_range_srt = srt_lines[i + 1]
-       text_raw_srt   = srt_lines[i + 2]
-       blank_srt      = srt_lines[i + 3]
+        audio_obj = AudioSegment.from_file(audio_filename).set_frame_rate(hparams.sampling_rate).set_channels(1)
+    
+        srt_filename = change_extension(audio_filename_in, "srt")
 
-       print(f"I: {idx} TR: {time_range_srt} T: {text_raw_srt}")
+        with open(srt_filename, "r") as f:
+            srt_lines = f.readlines()
 
-       range_srt = time_range_srt.split(" --> ")
-       start_time = range_srt2millis(range_srt[0])
-       end_time = range_srt2millis(range_srt[1])
+        out_lines_wav = list()
+        #out_lines_npy = list()
+        out_wav = list()
 
-       text = extract_text(text_raw_srt, speaker)
-       #print(f"Text: {text}")
-       if not text:
-           continue
+        if os.path.exists(index_filename):
+            with open(index_filename, "r") as f:
+                sequence = int(f.read())
+        else:
+            sequence = 0
 
-       #print(f"ST: {start_time} - {end_time}: {text}")
-       audio_chunk = audio_obj[start_time:end_time]
-       audio_chunk_filename_wav = f"chunk_{sequence:05}.wav"
-       audio_chunk_filename_npy = f"chunk_{sequence:05}.npy"
-       audio_chunk_full_path_wav = os.path.join(audio_out_dir, audio_chunk_filename_wav)
-       audio_chunk_full_path_npy = os.path.join(audio_out_dir, audio_chunk_filename_npy)
-       audio_chunk_rel_path_wav = os.path.join(audio_subdir, audio_chunk_filename_wav)
-       audio_chunk_rel_path_npy = os.path.join(audio_subdir, audio_chunk_filename_npy)
-       audio_chunk.export(audio_chunk_full_path_wav, format="wav")
-       wav2mel(audio_chunk_full_path_wav, audio_chunk_full_path_npy)
+        print(f"Begin index: {sequence}")
+        total_lines = len(srt_lines)
+        
 
-       out_lines_wav.append(f"{audio_chunk_rel_path_wav}| {text}")
-       out_lines_npy.append(f"{audio_chunk_rel_path_npy}| {text}")
-       sequence += 1
+        for i in range(0, total_lines, 4):
+            idx            = srt_lines[i]
+            time_range_srt = srt_lines[i + 1]
+            text_raw_srt   = srt_lines[i + 2]
+            blank_srt      = srt_lines[i + 3]
 
-    with open(dataset_filename_wav, "w") as f:
+            #print(f"I: {idx} TR: {time_range_srt} T: {text_raw_srt}")
+
+            range_srt  = time_range_srt.split(" --> ")
+            start_time = range_srt2millis(range_srt[0])
+            end_time   = range_srt2millis(range_srt[1])
+
+            text = extract_text(text_raw_srt, speaker)
+            if text:
+                text = text.strip()
+
+            #print(f"Text: {text}")
+            if not text:
+                continue
+
+            #print(f"ST: {start_time} - {end_time}: {text}")
+            audio_chunk = audio_obj[start_time - 100 : end_time + 100]
+            audio_chunk = effects.normalize(audio_chunk) 
+
+
+            audio_silence = AudioSegment.silent(duration=500)
+            audio_chunk = audio_silence + audio_chunk + audio_silence
+
+            audio_chunk_filename_wav = f"chunk_{sequence:05}.wav"
+            #audio_chunk_filename_npy = f"chunk_{sequence:05}.npy"
+            audio_chunk_full_path_wav = os.path.join(audio_out_dir, audio_chunk_filename_wav)
+            #audio_chunk_full_path_npy = os.path.join(audio_out_dir, audio_chunk_filename_npy)
+            audio_chunk_rel_path_wav = os.path.join(audio_subdir, audio_chunk_filename_wav)
+            #audio_chunk_rel_path_npy = os.path.join(audio_subdir, audio_chunk_filename_npy)
+            audio_chunk.export(audio_chunk_full_path_wav, format="wav")
+
+            #print(f"Writing: {audio_chunk_rel_path_wav} {audio_chunk_rel_path_npy}")
+            #with tempfile.TemporaryDirectory() as tmp_dir:
+            #    print(f"Writing: {audio_chunk_rel_path_wav}")
+                
+            #    tmp_filemame_wav = os.path.join(tmp_dir, audio_chunk_filename_wav)
+            #    audio_chunk.export(tmp_filemame_wav, format="wav")
+                
+            #    print(f"Processing: {audio_chunk_rel_path_wav}")
+            #    preprocess_audio(tmp_filemame_wav, 50000)
+            #    shutil.copy(tmp_filemame_wav, audio_chunk_full_path_wav)
+
+            #wav2mel(audio_chunk_full_path_wav, audio_chunk_full_path_npy)
+    
+            text_wo_nums = extract_nums_from_text(text, lang)
+            print(f"Text w/o nums: {text_wo_nums}")
+            out_lines_wav.append(f"{audio_chunk_rel_path_wav}|{text}|{text_wo_nums}\n")
+            out_wav.append(audio_chunk_rel_path_wav)
+            #out_lines_npy.append(f"{audio_chunk_rel_path_npy}|{text}")
+            sequence += 1
+
+    
+
+    with open(dataset_filename_wav, "a") as f:
         f.writelines(out_lines_wav)
 
-    with open(dataset_filename_npy, "w") as f:
-        f.writelines(out_lines_npy)
+    #with open(dataset_filename_npy, "a") as f:
+    #    f.writelines(out_lines_npy)
+
+    with open(index_filename, "w") as f:
+        f.write(str(sequence))
+
+    print(f"Total {len(out_lines_wav)} chunks, next index: {sequence}")
+
+def get_parser():
+    parser = argparse.ArgumentParser("gen_dataset",
+                                     description="Generate TTS datasets from audios and srt")
+    parser.add_argument("-i",
+                        "--audio_path",
+                        help="Input audio file")
+    parser.add_argument("-n",
+                        "--speaker_num",
+                        type=int,
+                        help="Speaker number selected")
+    parser.add_argument("-l",
+                        "--lang",
+                        default="es",
+                        help="Language selected")
+    parser.add_argument("-o",
+                        "--outdir",
+                        default="result",
+                        help="Dataset output directory")
+    parser.add_argument("-r",
+                        "--noise_remove",
+                        type=bool,
+                        default=False,
+                        help="Isolate vocal part of audio using demucs neural filter")
+
+    return parser
+
+def main(opts=None):
+    parser = get_parser()
+    args = parser.parse_args(opts)
+    audio_srt_to_chunks(args.speaker_num, args.audio_path, args.lang, args.outdir, noise_remove=args.noise_remove)    
 
 
-if len(sys.argv)!=4:
-    print(f"Usage: {sys.argv[0]} num_speaker audio_path outdir")
-    sys.exit(1)
+
+#if len(sys.argv) < 3:
+#    print(f"Usage: {sys.argv[0]} num_speaker audio_path [lang] [outdir]")
+#    sys.exit(1)
 
 init_globals()
+main()
 
-audio_srt_to_chunks(sys.argv[1], sys.argv[2], sys.argv[3])
+#lang   = sys.argv[3] if len(sys.argv)>=4 else "es"
+#outdir = sys.argv[4] if len(sys.argv)>=5 else "result"
+
+#audio_srt_to_chunks(sys.argv[1], sys.argv[2], lang, outdir)
 
 
